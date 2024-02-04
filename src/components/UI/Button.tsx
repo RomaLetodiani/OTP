468a1d13ec1b8fae44a2104ea9029b524a67be65
RomaLetodiani <roman.letodiani03@gmail.com>
2024-02-04 18:29:05 +0400

    refactored Button and Input Components

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -4,16 +4,16 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
 }
 
-const Button = (props: Props) => {
+const Button = ({ text, className, ...rest }: Props) => {
   return (
     <button
-      {...props}
+      {...rest}
       className={twMerge(
         'border-light-blue border shadow-md rounded-lg px-5 py-1 text-sm font-bold text-white',
-        props.className
+        className
       )}
     >
-      {props.text}
+      {text}
     </button>
   );
 };
--- src/components/UI/Input.tsx
+++ src/components/UI/Input.tsx
@@ -4,41 +4,50 @@ export type InputProps = {
   inputClass?: string;
   value?: string;
   id?: string;
+  inputRef?: React.Ref<HTMLInputElement>;
   hasError?: boolean;
   errorMessage?: string;
   type?: string;
   label?: string;
   focus?: boolean;
+  onClick?: () => void;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
   onBlur?: () => void;
   onFocus?: () => void;
 };
 
 const Input = (props: InputProps) => {
+  const labelClassName = `absolute left-3 pointer-events-none transition-all ${
+    props.focus
+      ? 'text-[10px] top-1'
+      : 'text-sm translate-x-0 -translate-y-1/2 top-1/2'
+  } ${props.hasError ? 'text-red-500' : ''}`;
+
+  const inputClassName = twMerge(
+    'w-full rounded-lg border border-gray-300 outline-none text-sm',
+    props.inputClass,
+    props.hasError && 'border-red-500 text-red-500'
+  );
   return (
-    <div className="flex flex-col relative w-full">
-      <label
-        className={`absolute left-3 pointer-events-none transition-all ${
-          props.focus
-            ? 'text-[10px] top-1'
-            : 'text-sm translate-x-0 -translate-y-1/2  top-1/2'
-        } ${props.hasError && 'text-red-500'}`}
-        htmlFor={props.id}
-      >
-        {props.hasError ? props.errorMessage : props.label}
-      </label>
+    <div className="relative w-full">
+      {props.label && (
+        <label className={labelClassName} htmlFor={props.id}>
+          {props.hasError ? props.errorMessage : props.label}
+        </label>
+      )}
       <input
+        autoComplete="off"
         type={props.type ?? 'text'}
         id={props.id}
+        ref={props.inputRef}
         onChange={props.onChange}
+        onClick={props.onClick}
+        onKeyDown={props.onKeyDown}
         onBlur={props.onBlur}
         onFocus={props.onFocus}
         value={props.value}
-        className={twMerge(
-          'rounded-lg border border-gray-300 outline-none text-sm',
-          props.inputClass,
-          `${props.hasError && 'border-red-500 text-red-500'}`
-        )}
+        className={inputClassName}
       />
     </div>
   );
